bf424f0ca8ee87cbb4de3fb8136aabc027a17945
Janey Slinger <janeyslinger@gmail.com>
2020-05-04 11:00:20 +0100

    Updates

---
+++ precache-manifest.911a8ef6ba9bd70e71cd8abc45d54d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77f00decc7d2d3c1729b7c12f0035bc",
+    "url": "/tic-tac-toe-app/index.html"
+  },
+  {
+    "revision": "3545ab0ee8a13225adbc",
+    "url": "/tic-tac-toe-app/static/css/main.f9f3732d.chunk.css"
+  },
+  {
+    "revision": "22cd87cacfcb610dbf3c",
+    "url": "/tic-tac-toe-app/static/js/2.54a0c577.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-app/static/js/2.54a0c577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3545ab0ee8a13225adbc",
+    "url": "/tic-tac-toe-app/static/js/main.0b658ecc.chunk.js"
+  },
+  {
+    "revision": "49d6f92ede405a14ea1b",
+    "url": "/tic-tac-toe-app/static/js/runtime-main.f6cae35e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
